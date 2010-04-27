56e337baee02d63eeb48f4de88c14e59075da5a8
Zachary D. Shaw <zdsbs@yahoo.com>
2010-04-26 21:43:19 -0400

    now going from arabic to roman

--- roman_numerals.clj
+++ roman_numerals.clj
@@ -1,17 +1,38 @@
 (ns roman-numerals
   (:require [clojure.contrib.str-utils2 :as s]))
 
-(defn convert-single-digit [roman]
-  (cond 
-    (= \I roman) 1
-    (= \V roman) 5
-    (= \X roman) 10
-    (= \L roman) 50
-    (= \C roman) 100
-    (= \D roman) 500
-    (= \M roman) 1000
-    :else roman)
+(defn roman-to-arabic-single-digit [roman]
+  (condp = roman
+    \I 1
+    \V 5
+    \X 10
+    \L 50
+    \C 100
+    \D 500
+    \M 1000
+    roman)
   )
+
+(defn get-closest-num [arabic]
+  (some #(if (<= % arabic) % false) [1000 900 500 400 100 90 50 40 10 9 5 4 1])
+  )
+
+(defn arabic-to-roman-single-digit [arabic]
+  (condp = arabic
+    1 "I"
+    4 "IV"
+    5 "V"
+    9 "IX"
+    10 "X"
+    40 "XL"
+    50 "L"
+    90 "XC"
+    100 "C"
+    400 "CD"
+    500 "D"
+    900 "CM"
+    1000 "M"
+  ))
 
 (defn replace-subtractives [roman]
   (let [swappers [["IV" "IIII"] 
@@ -23,6 +44,15 @@
         swapper (fn [in [old new]] (s/replace in old new))]
         (reduce swapper roman swappers)))
         
-(defn convert [roman]
-  (reduce + (map convert-single-digit (replace-subtractives roman))))
+(defn roman-to-arabic [roman]
+  (reduce + (map roman-to-arabic-single-digit (replace-subtractives roman))))
+
+(defn arabic-to-roman [arabic]
+  (loop [arabic-num arabic
+       roman ""]
+        (let [closest-num (get-closest-num arabic-num)]
+          (if (= 0 arabic-num)
+            roman
+            (recur (- arabic-num closest-num) (str roman (arabic-to-roman-single-digit closest-num)))))))
+
 
--- roman_numerals_test.clj
+++ roman_numerals_test.clj
@@ -4,27 +4,27 @@
 ;TDD is very much a decision of where to start
 ;The first test you write will greatly impact your direction
 
-(deftest convert-single-digits
-         (is (= 1 (convert "I")))
-         (is (= 5 (convert "V")))
-         (is (= 10 (convert "X")))
-         (is (= 50 (convert "L")))
-         (is (= 100 (convert "C")))
-         (is (= 500 (convert "D")))
-         (is (= 1000 (convert "M")))
+(deftest roman-to-arabic-single-digits
+         (is (= 1 (roman-to-arabic "I")))
+         (is (= 5 (roman-to-arabic "V")))
+         (is (= 10 (roman-to-arabic "X")))
+         (is (= 50 (roman-to-arabic "L")))
+         (is (= 100 (roman-to-arabic "C")))
+         (is (= 500 (roman-to-arabic "D")))
+         (is (= 1000 (roman-to-arabic "M")))
          )
 
 (deftest addition-digits
-         (is (= 2 (convert "II")))
-         (is (= 3 (convert "III")))
-         (is (= 1601 (convert "MDCI")))
+         (is (= 2 (roman-to-arabic "II")))
+         (is (= 3 (roman-to-arabic "III")))
+         (is (= 1601 (roman-to-arabic "MDCI")))
          )
 
 (deftest with-subtraction
-         (is (= 24 (convert "XXIV")))
-         (is (= 1967 (convert "MCMLXVII")))
-         (is (= 999 (convert "CMXCIX")))
-         (is (= 1444 (convert "MCDXLIV")))
+         (is (= 24 (roman-to-arabic "XXIV")))
+         (is (= 1967 (roman-to-arabic "MCMLXVII")))
+         (is (= 999 (roman-to-arabic "CMXCIX")))
+         (is (= 1444 (roman-to-arabic "MCDXLIV")))
          )
 
 (deftest replace-subtractives-test
@@ -37,5 +37,30 @@
         (is (= "DCCCC"  (replace-subtractives "CM")))
          )
 
+(deftest arabic-to-roman-single-digits
+         (is (= "I" (arabic-to-roman 1)))
+         (is (= "V" (arabic-to-roman 5)))
+         (is (= "X" (arabic-to-roman 10)))
+         (is (= "L" (arabic-to-roman 50)))
+         (is (= "C" (arabic-to-roman 100)))
+         (is (= "D" (arabic-to-roman 500)))
+         (is (= "M" (arabic-to-roman 1000)))
+         )
+
+
+(deftest arabic-to-roman-results-in-roman-addition
+         (is (= "II" (arabic-to-roman 2)))
+         (is (= "III" (arabic-to-roman 3)))
+         (is (= "VI" (arabic-to-roman 6)))
+         (is (= "MDCI" (arabic-to-roman 1601)))
+         )
+
+(deftest arabic-to-roman-results-in-roman-sub
+         (is (= "IV" (arabic-to-roman 4)))
+         (is (= "XXIV" (arabic-to-roman 24)))
+         (is (= "MCMLXVII" (arabic-to-roman 1967)))
+         (is (= "CMXCIX" (arabic-to-roman 999)))
+         (is (= "MCDXLIV" (arabic-to-roman 1444)))
+         )
 
 (run-tests 'roman-numerals-test)
